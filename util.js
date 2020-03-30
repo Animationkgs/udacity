be9344d7959ef3e8bf564ffc198ce1f2871f4524
Animationkgs <62798552+Animationkgs@users.noreply.github.com>
2020-03-30 12:53:28 +0530

    Add files via upload

--- util.js
+++ util.js
@@ -1,102 +1,215 @@
 
-function lesson03(x,n) {
-  x.lesson= 3;
-  x.number= n;
-}
 
-
-
-
-
-function pretty(x) { return x.split( '\n' ).join( '<br>' ); }
-function fill(id, x) { document.getElementById(id).innerHTML= pretty(x); }
-
-function setupids(ids) {
-  var n= 0;
-  while (n < ids.length) {
-	  var t= ids[n];
-	  fill( t[0], t[1] );
-	  n++;
-  }
-}
-
-function makeButton(id) {
-  return ` <button id= ${id} 
-                     onclick= "my[${id}].show()"> 
-		       test 
-	     </button> <br> `;
-}
-
-function clearmeButton(id) {
-  var code= ` function () {
-    document.getElementById('${id}').innerHTML= '${id}';
-  } `;
-  return `<button id="${id}" onclick="(${code})()">Clearme</button>`;
-}
-
-function makeDiv(id) { return ` <div id="${id}"> ${id} </div> `; }
-function fillDiv(id,x) { return ` <div id="${id}"> ${x} </div> `; }
-function makeAnonymousDiv(x) { return ` <div> ${x} </div> `; }
-
-
-function makeTable(x) {
-  var s= x.split('\n').map(makeTablerow).join( '' );
-  return ` <table valign="top"> ${s} </table> `;
-}
-
-function makeTd(x) { return ` <td> ${x} </td> `; }
-
-function makeTablerow(x) {
-  var cells= x.split(',').map( makeTd );
-  return ` <tr> ${cells.join('')} </tr> `;
-}
-
-
-function clearmeDiv2(id,s) {
-  var d=  fillDiv(id,s);
-  var b= clearmeButton(id);
-  var box= [d, b ];
-  return box.toString();
-}
-
-
-function clearmeDiv(id) {
-  var d=  makeDiv(id);
-  var s= clearmeButton(id);
-  var box= [d, s ];
-  return box.toString();
-}
-
-
-function iframe(vid) {
-	var ret= ` <iframe width="440" height="248" src="https://www.youtube.com/embed/${vid}?rel=0&amp;autoplay=1&mute=1&cc_load_policy=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> `;
-	return ret;
-}
-
-
-
-function setupstyle() {
-   var style= `
+var style= `
 div { background-color: green; border: 10px; }
 div div { background-color: pink; border: 4px; }
 div div div { background-color: maroon; border: 16px; }
 img { width: 500px; height: auto; }
 `;
 
-   var element = document.createElement("style");
-   element.innerHTML= style;
-   document.head.appendChild(element);
+var element= function () {
+	return {
+   e : null, 
+   create : function (eT) { this.e= document.createElement(eT); return this; },
+   find : function (id) { this.e= document.getElementById(id); return this; },
+   init: function (e) { this.e= e; return this; },
+   setid : function (id) { this.e.id= id; return this; },
+   appendTo: function (p) { p.e.appendChild(this.e); return this; },
+   append: function (e) { this.e.appendChild(e.e); return this; },
+   setme: function (x) { this.e.innerHTML= x; return this; }, 
+   setonclick: function (f) { this.e.onclick= f; return this; }
+	};
+};
+
+var dom= {
+   make : function (eT) { return element().create(eT); },
+   find : function (id) { return element().find(id); },
+   init : function (e)  { return element().init(e); },
+   body : element().init( document.body ),
+   head : element().init( document.head ),
+   div  : function () {  return element().create('div'); },
+   richdiv : function (id,s) { return this.div().setid(id).setme(s); },
+   button  : function () { return element().create('button'); },
+   richbutton  : function (id,f,x)  {
+	return this.button().setid(id).setonclick(f).setme(x);
+   },
+   style  : function (x) { return element().create('style').setme(x); }, 
+   img  : function () { return element().create('img'); }, 
+   title  : function (x) { return element().create('title').setme(x); }
+};
+
+alert(1);
+
+dom.body.append( dom.richdiv('d1','hi') );
+var box= function (id,s) {
+	return {
+   onclick1 : function () { dom.find(id).setme(id); },
+   onclick2 : function () { dom.find(id).setme(s); },
+   b1id : id+'Clearme',
+   b2id : id+'Fillme',
+   b1 : function () { return dom.richbutton (this.b1id, this.onclick1, 'Clear me'); },
+   b2 : function () { return dom.richbutton (this.b2id, this.onclick2, 'Fill me'); },
+   div : function () { return dom.richdiv (id,s); }
+	};
+};
+
+
+
+var util= {
+   zipme : function (a,b) { return a.map( function(e, i) { return [e, b[i]]; } ); },
+   pad   : function (x)   { return x.toString().padStart(2,"0"); }, 
+   parse : function (s)   { return s.split(/\s+/); }, 
+   pretty: function (x)   { return x.split( '\n' ).join( '<br>' ); }, 
+   fill  : function (id, x) { document.getElementById(id).innerHTML= this.pretty(x); },
+   setme : function (id, x) { document.getElementById(id).innerHTML= x; }
+};
+
+function lesson03(x,n) { x.lesson= 3; x.number= n; }
+
+/*
+var html= {
+   me :   this,
+   img :  function (src) { return ` <img src='${src}'> `; }, 
+   div :  function (id,x) { return ` <div id="${id}"> ${x} </div> `; }, 
+   anonymousdiv : function (x) { return ` <div> ${x} </div> `; }, 
+   iframe : function (vid) {
+	return ` <iframe width="440" height="248" src="https://www.youtube.com/embed/${vid}?rel=0&amp;autoplay=1&mute=1&cc_load_policy=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> `;
+   },
+   button : function (id, code, caption) {
+	return ` <button id= ${id} onclick= "(${code})()"> ${caption} </button> `;
+   }
+};
+*/
+
+/*
+
+
+var add= {
+   div: function (id, s, p) {
+      var div = document.getElementById( "initfields" );
+      div.innerHTML=  div.innerHTML.concat(text.div(id,id + '= ' + s))
+}
+};
+
+
+
+function elementButton(id,onclick,x) {
+   var ret= document.createElement('button');
+   ret.id= id; ret.onclick= onclick; ret.innerHTML= x;
+   return ret;
+}
+text.table= {
+   td : function (x) { return ` <td> ${x} </td> `; }, 
+   tr : function (x) {
+      var cells= x.split(',').map( this.td ).join( '' );
+      return ` <tr> ${cells} </tr> `;
+   },
+   get : function (x) {
+      var rows= x.split('\n').map( this.tr ).join( '' );
+      return ` <table valign="top"> ${rows} </table> `;
+   }
+};
+
+
+
+box= {
+   x: '1';
+   onclick1 : function () { setme(id,id); }
+   onclick2 : function () { setme(id,'1'); }
+   button : function (id) {
+	   create( 'button', 
+	return ` <button id= "${id}clearme" onclick= "this.onclick1"> Clear </button> `;
+   },
+   div    : function (id,s) {
+      return [ text.div(id,s), this.button(id) ].join(' ');
+   }
+};
+
+text.fillmebutton= function (id, s) {
+  var code= ` function () { document.getElementById('${id}').innerHTML= \`${s}\`; } `;
+  return text.button (id+'fillme', code, 'Fillme');
+};
+
+
+function getBox(id,data) {
+   var ret= {
+      boxid : id+'box',
+      divid: id,
+      data: data,
+      clearme: function() { document.getElementById(this.divid).innerHTML= this.divid; },
+      fillme: function() { document.getElementById(this.divid).innerHTML= this.data; },
+      button1: elementButton (this.divid+'clearme', this.clearme, 'Clearme'),
+      button2: elementButton (this.divid+'fillme', this.fillme, 'Fillme'), 
+      div: elementDiv (this.divid, this.data),
+   };
+	document.getElementById('fields').appendChild(ret.div);
+}
+add.box= function ( id, s ) { add.div (id+'box', text.clearme.div(id,s)); }
+add.video= function ( vid, n ) { add.box ('embed' + pad(n), text.iframe(vid)); }
+add.subtitle= function ( x, n ) { add.box ( 'subtitle' + pad(n), pretty(x) ); }
+
+
+
+setup.fields= function (x) {
+alert(1);
+alert(2);
+   add.div ( "MessageMeaning", x.messagemeaning );
+   parse(x.vids).map( add.video );
+	var vids= parse(x.vids);
+document.getElementById('fields').appendChild( elementDiv ('vid01', text.iframe(vids[0])) );
+   x.subs.map( add.subtitle );
+   x.subsmeaning.map( add.subsmeaning );
+   if ( 'quiz' in x && x.quiz!=null ) {
+     var q= x.quiz;
+     add.box ( "QuizImage", text.img(q.image) );
+     add.div ( "QuizOCR", q.ocr );
+     add.div ( "quizSolution", q.solution );
+     add.div ( "quizSolutionFeedback", q.solutionfeedback );
+   }
+}
+*/
+
+/*
+function add (id, s) {
+   var p= dom.find('initfields');
+   dom.div().setid(id).setme(id+"= "+s).appendTo(p);
 }
 
-function setuptitle(title) {
-  var title= `<title> ${title} </title>`;
-  var element = document.createElement("title");
-  element.innerHTML= title;
-  document.head.appendChild(element);
-}
 
-
-function pad(x) { return x.toString().padStart(2,"0"); }
+var setup= new Object();
+setup.fields= function (x) {
+	alert(1);
+   add ( "navigate", [x.prev, x.next] );
+   add ( "CourseTitle", x.title );
+   add ( "Wiki", x.wiki );
+   add ( "URL", x.url );
+   add ( "Concept", x.concept );
+   add ( "VideoIds", x.vids );
+	x.message= 'test';
+   add ( "MessageBox", x.message );
+   var p= dom.find("MessageBox");
+   p.append( dom.div().setid( 'Message' ).setme( 'message' ) );
+	var b1= dom.button().setid('MessageClearme').setme('Clear me');
+	var b2= dom.button().setid('MessageFillme').setme('Fill me');
+	var b= box('Message','data');
+	b1.e.onclick= b.onclick1;
+	b2.e.onclick= b.onclick2;
+   p.append(b1);
+   p.append(b2);
+};
+setup.page= function (x) {
+   var p= dom.head;
+   p.append(dom.style(style));
+   var t= util.pad(x.lesson);
+   x.name= `${t}${util.pad(x.number)}`;
+   p.append(dom.title(x.name));
+  var prev= t+ util.pad(x.number-1);
+  var next= t+ util.pad(x.number+1);
+  x.prev= `<a href= "${prev}.html"> prev </a>`;
+  x.next= `<a href= "${next}.html"> next </a>`;
+  //x.subs= zipme( x.subs, x.subsmeaning ).map(objectSubtitle);
+  setup.fields(x);
+};
 
 
 function video(vid, subs) {
@@ -107,76 +220,4 @@ function video(vid, subs) {
   ret.meaning= 'meaning';
   return ret;
 }
-
-function parse(s) { return s.split(/\s+/); }
-
-function createfields(x) {
-
-   var fields= [];
-   var boxes= [];
-   if (x.quiz!=null) { fields.push( quizfields ); boxes.push( quizboxes ); }
-   fields= fields.join(" "); boxes= boxes.join(" ");
-   fields= parse(fields);
-   fields= fields.map(makeDiv);
-   document.getElementById( "fields" ).innerHTML= fields.join('\n');
-}
-
-function add(id, s) {
-   s= fillDiv(id,s);
-   var str1= document.getElementById( "initfields" ).innerHTML;
-   document.getElementById( "initfields" ).innerHTML= str1.concat(s);
-}
-function addbox( id, s ) {
-   s=  clearmeDiv2( id, s );
-   add ( id+'box', s );
-}
-
-function addvid ( x, index ) {
-   var id= 'embed' + pad(index);
-   var s= iframe( x );
-   addbox ( id, s );
-}
-
-function addsubtitle ( x, n ) {
-   var id= 'subtitle' + pad(n);
-   var s=  pretty(x);
-   addbox ( id, s );
-}
-
-function setupfields(x) {
-   add ( "navigate", [x.prev, x.next] );
-   add ( "title", "Course Title= " + x.title );
-   add ( "wiki", "Wiki= " + x.wiki );
-   add ( "url", "URL= " + x.url );
-   add ( "concept", "Concept= " + x.concept );
-   add ( "videos", "Vids= " + x.vids );
-   addbox ( "message", "Message= " + x.message );
-   add ( "messagemeaning", "Message Meaning= " + x.messagemeaning );
-   parse(x.vids).map( addvid );
-   x.subs.map( addsubtitle );
-   if ( 'intro' in x && x.intro!=null && 'meaning' in x.intro)
-     add( "introvideosubtitlesmeaning", "Intro Video Subtitles Meaning= " + x.intro.meaning );
-   if ( 'answer' in x && x.answer!=null && 'meaning' in x.answer)
-     add( "answervideosubtitlesmeaning", "Answer Video Subtitles Meaning= " + x.answer.meaning );
-   if ( 'quiz' in x && x.quiz!=null ) {
-     var q= x.quiz;
-     addbox ( "quiz", q.image );
-     add ( "quizocr", q.ocr );
-     add ( "quizsolution", "Solution= " + q.solution );
-     add ( "quizsolutionfeedback", "Solution Feedback= " + q.solutionfeedback );
-   }
-}
-
-
-function setuppage(x) {
-  setupstyle();	
-  var t= pad(x.lesson);
-  x.name= `${t}${pad(x.number)}`;
-  setuptitle(x.name)
-  var prev= t+ pad(x.number-1);
-  var next= t+ pad(x.number+1);
-  x.prev= `<a href= "${prev}.html"> prev </a>`;
-  x.next= `<a href= "${next}.html"> next </a>`;
-  setupfields(x);
-}
-
+*/
